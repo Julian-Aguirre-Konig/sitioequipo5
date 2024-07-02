dec17b0d2933db28686e1d8c57a1ba46f7f48958
Julian-Aguirre-Konig <jaguirrekonig@uade.edu.ar>
2024-07-01 23:53:10 -0300

    modified:   juegos.html 	modified:   scripts.js 	modified:   styles.css

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    const slider = document.querySelector('.slider');
+    const slideWidth = slides[0].clientWidth;
+    slider.style.transform = `translateX(${-index * slideWidth}px)`;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+// Auto slide every 15 seconds
+setInterval(nextSlide, 15000);
